b95543fd6fea90b4eed074099d39c7257e135837
oskyar <oskyar@gmail.com>
2014-03-18 01:37:05 +0100

    Se añade script para ir probando, por ahora crea ficheros .csv

--- pruebas/matriculas-becas.pl
+++ pruebas/matriculas-becas.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 
 use File::Slurp qw(read_file);
+use Text::CSV;
 
 use v5.014;
 
@@ -8,8 +9,13 @@ use JSON;
 
 my %data;
 
+my $csv = Text::CSV->new ( { binary => 1 } )  # should set binary attribute.
+                 or die "Cannot use CSV: ".Text::CSV->error_diag ();
+
 while (@ARGV) {
+
     my $file_name = shift @ARGV;
+    open( SALIDA, ">$file_name".".csv") or die "No se puede abrir $file_name";
     my $file_data = read_file($file_name);
 
     my ($year) = ($file_name =~ /(\d+)\.txt/);
@@ -18,11 +24,14 @@ while (@ARGV) {
 
     my ($mec) = ($file_data =~ /BECARIO .+? (\d+)/s);
 
-    my ($mujeres) = ($)
+ #   my ($mujeres) = ($)
 
     $data{$year} = { alumnos => $alumnos,
 		     ordinarios => $ordinarias,
 		     mec => $mec };
 
+    close SALIDA;
+
 }
-say(to_json\%data));
+ print Text::CSV->VERSION;
+#say(to_json\%data));
