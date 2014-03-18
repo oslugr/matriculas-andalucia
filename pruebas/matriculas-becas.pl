ded95e8dc0de972d36d1515fa9d89c7ec4229a5f
oskyar <oskyar@gmail.com>
2014-03-18 11:15:31 +0100

    Ya se puede coger de los .txt, Año, Alumnos, Varones, Mujeres, Matrículas Ordinarias y Becas

--- pruebas/matriculas-becas.pl
+++ pruebas/matriculas-becas.pl
@@ -6,17 +6,32 @@ use Text::CSV;
 use v5.014;
 
 use JSON;
+use strict;
 
 my %data;
 
 my $csv = Text::CSV->new ( { binary => 1 } )  # should set binary attribute.
                  or die "Cannot use CSV: ".Text::CSV->error_diag ();
 
+#El archivo se reescribe siempre que se ejecute el programa
+open( SALIDA, ">matriculas-becas-ugr.csv") or die "No se puede abrir matriculas-becas-ugr.csv" . ".csv";
+#Cabecera del documento
+print SALIDA "Año, Alumnos, Varones, Mujeres, Ordinarios, Becas \n";
+
 while (@ARGV) {
 
     my $file_name = shift @ARGV;
-    open( SALIDA, ">$file_name".".csv") or die "No se puede abrir $file_name";
     my $file_data = read_file($file_name);
+
+#Para formatear el archivo entero en csv (Sin terminar)
+#    open( ENTRADA, "$file_name") or die "No se puede abrir $file_name";
+
+
+#    $file_data =~ s/[\|\:]/,/g;
+#    $file_data =~ s/_/ /g;
+
+#    print SALIDA $file_data;
+ 
 
     my ($year) = ($file_name =~ /(\d+)\.txt/);
     
@@ -24,14 +39,17 @@ while (@ARGV) {
 
     my ($mec) = ($file_data =~ /BECARIO .+? (\d+)/s);
 
- #   my ($mujeres) = ($)
+    my ($varones, $mujeres) = ($file_data =~ /VARONES:.+?(\d+)\s+.+?MUJERES:.+?(\d+)/s);
 
-    $data{$year} = { alumnos => $alumnos,
-		     ordinarios => $ordinarias,
-		     mec => $mec };
+#    $data{$year} = { alumnos => $alumnos,
+#		     ordinarios => $ordinarias,
+#		     mec => $mec };
 
-    close SALIDA;
+    print SALIDA $year.",".$alumnos.",".$varones.",".$mujeres.",".$ordinarias.",".$mec."\n";
+
 
 }
- print Text::CSV->VERSION;
+
+close SALIDA;
+
 #say(to_json\%data));
